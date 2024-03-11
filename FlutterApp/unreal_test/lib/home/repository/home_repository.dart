7892b7038afff3fb6c2b3cb7caea98dc7d0c780b
jeWosch <163006217+jeWosch@users.noreply.github.com>
2024-03-11 16:44:03 +0100

    Clean repo

--- FlutterApp/unreal_test/lib/home/repository/home_repository.dart
+++ FlutterApp/unreal_test/lib/home/repository/home_repository.dart
@@ -7,25 +7,8 @@ class HomeRepository {
     Uri.parse('wss://YOURLINK'),
   );
 
-
-  // Selbst einen Stream verwalten.
-  // Andere außerhalb wie bspw. ein Bloc können auf diesen hören.
-  //final BehaviorSubject<Data> _streamForBloc = BehaviorSubject();
-
-  // Listener auf den Websocket
-  //StreamSubscription<RestData> _websocketListener;
-
-
   HomeRepository();
 
-  // Die Funktion um etwas über den Websocket an den Server zu senden.
   void putOnStream(RestList data) => _websocketChannel.sink.add(jsonEncode(data.toJson()));
-
-  // Falls bloc oder andere auf Antworten vom Server über den
-  // Websocket reagieren sollen. (Wird vom Repo verwaltet.)
-  //Stream<Data> get _streamForBloc => _websocketStream.stream;
-
-  // Ein Beispiel, wenn etwas auf den Stream vom Repo explizit warten will.
-  //Future<ResponseData> getWebsocketMessage() async => await _websocketStream.stream.first;
 
 }
